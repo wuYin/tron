cb383610a86df7e1698dc1c5c11615f116d33dcb
wuYin <wuYinPost@gmail.com>
2019-03-15 15:51:27 +0800

    添加包 seq 管理

--- config.go
+++ config.go
@@ -3,20 +3,22 @@ package tron
 import "time"
 
 type Config struct {
-	ReadBufSize   int           // 读缓冲区大小
-	WriteBufSize  int           // 写缓冲区大小
-	ReadChanSize  int           // 异步读 channel 大小
-	WriteChanSize int           // 异步写 channel 大小
-	IdleDuration  time.Duration // 连接的最大空闲时间
+	ReadBufSize   int            // 读缓冲区大小
+	WriteBufSize  int            // 写缓冲区大小
+	ReadChanSize  int            // 异步读 channel 大小
+	WriteChanSize int            // 异步写 channel 大小
+	IdleDuration  time.Duration  // 连接的最大空闲时间
+	PacketManager *PacketManager // 包序号管理
 }
 
-func NewConfig(rBufSize, wBufSize int, rChSize, wChSize int, d time.Duration) *Config {
+func NewConfig(rBufSize, wBufSize int, rChSize, wChSize int, maxSeq int32, idle time.Duration) *Config {
 	c := &Config{
 		ReadBufSize:   rBufSize,
 		WriteBufSize:  wBufSize,
 		ReadChanSize:  rChSize,
 		WriteChanSize: wChSize,
-		IdleDuration:  d,
+		IdleDuration:  idle,
+		PacketManager: NewPacketManager(maxSeq),
 	}
 	return c
 }
---
+++ packet_manager.go
@@ -0,0 +1,71 @@
+package tron
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type PacketManager struct {
+	maxSeq int32
+	curSeq int32
+	locks  []*sync.Mutex
+	groups []map[int32]chan interface{}
+}
+
+const (
+	MAX_CONCUR = 10 // 并发级别
+)
+
+// 并发级别决定分组
+func NewPacketManager(maxSeq int32) *PacketManager {
+	m := &PacketManager{
+		maxSeq: maxSeq,
+		curSeq: 0,
+		locks:  make([]*sync.Mutex, MAX_CONCUR),
+		groups: make([]map[int32]chan interface{}, MAX_CONCUR),
+	}
+
+	avgMaxSeq := maxSeq / MAX_CONCUR // 每个底层 manager 分配到的 seq 长度
+	for i := 0; i < MAX_CONCUR; i++ {
+		m.locks[i] = &sync.Mutex{}
+		m.groups[i] = make(map[int32]chan interface{}, avgMaxSeq) // 分配内存直接使用
+	}
+
+	return m
+}
+
+// 记录一个新的 packet
+func (m *PacketManager) Attach(newSeq int32, sigCh chan interface{}) {
+	l, g := m.group(newSeq)
+	l.Lock()
+	g[newSeq] = sigCh
+	l.Unlock()
+}
+
+// 标记 seq 已处理
+func (m *PacketManager) Detach(oldSeq int32, res interface{}) {
+	l, g := m.group(oldSeq)
+	l.Lock()
+	defer l.Unlock()
+
+	fmt.Println("detached", oldSeq, string(res.([]byte)))
+	if ch, ok := g[oldSeq]; ok {
+		ch <- res
+		close(ch)
+		delete(g, oldSeq)
+	}
+}
+
+// 获取下一个可分配的 seq
+func (m *PacketManager) NextSeq() int32 {
+	next := atomic.AddInt32(&m.curSeq, 1)
+	return next % m.maxSeq // 轮回使用
+}
+
+func (m *PacketManager) group(seq int32) (lock *sync.Mutex, group map[int32]chan interface{}) {
+	g := seq % MAX_CONCUR
+	lock = m.locks[g]
+	group = m.groups[g]
+	return
+}
