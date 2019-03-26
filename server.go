c3801888ef80f96946da9777817193b8447a4c35
wuYin <wuYinPost@gmail.com>
2019-03-26 14:12:08 +0800

    简化代码

--- server.go
+++ server.go
@@ -10,8 +10,8 @@ type Server struct {
 	address   string
 	handler   func(worker *Client, p *Packet)
 	conf      *Config
-	living    bool
-	shutdown  chan struct{}
+	closed    bool
+	closeCh   chan struct{}
 	keepAlive time.Duration
 	codec     Codec
 }
@@ -20,15 +20,16 @@ func NewServer(addr string, conf *Config, serverCodec Codec, f func(worker *Clie
 	s := &Server{
 		address:   addr,
 		handler:   f,
-		living:    true,
+		closed:    false,
 		conf:      conf,
-		shutdown:  make(chan struct{}, 1), // 这里是缓冲 channel
+		closeCh:   make(chan struct{}, 1),
 		keepAlive: 5 * time.Second,
 		codec:     serverCodec,
 	}
 	return s
 }
 
+// 启动
 func (s *Server) ListenAndServe() error {
 	addr, err := net.ResolveTCPAddr("tcp4", s.address)
 	if err != nil {
@@ -42,30 +43,26 @@ func (s *Server) ListenAndServe() error {
 		return err
 	}
 
-	liver := NewLiveListener(listener, s.shutdown, 5*time.Second) // 保持 5s 连接
-	go s.run(liver)
+	liver := NewLiveListener(listener, s.closeCh, 5*time.Second) // 保持 5s 连接
+	go func(l *LiveListener) {
+		for !s.closed {
+			conn, err := l.AcceptTCP()
+			if err != nil {
+				logx.Error(err)
+				continue
+			}
+
+			// 将连接分发给 server worker 处理
+			serverWorker := NewClient(conn, s.conf, s.codec, s.handler)
+			serverWorker.ReadWriteAndHandle()
+		}
+	}(liver)
 	return nil
 }
 
 // 将服务器的连接关闭，不再接受新连接
 func (s *Server) Shutdown() {
-	s.living = false
-	s.shutdown <- struct{}{} // 立刻停止
+	s.closed = true
+	s.closeCh <- struct{}{} // 立刻停止
 	logx.Debug("shutdown...")
-}
-
-// run 服务器直到手动不接受新连接
-func (s *Server) run(l *LiveListener) error {
-	for s.living {
-		conn, err := l.AcceptTCP()
-		if err != nil {
-			logx.Error(err)
-			continue
-		}
-
-		// 将连接分发给 server worker 处理
-		serverWorker := NewClient(conn, s.conf, s.codec, s.handler)
-		serverWorker.ReadWriteAndHandle()
-	}
-	return nil
 }
